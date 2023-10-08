4b0c70b8d6cf76ad6186835e8859e85542fff76f
RanaHisham18 <hishamrana00@gmail.com>
2023-10-08 08:27:27 +0300

    add ViewBinding

--- app/src/main/java/com/example/myradio/MainActivity.kt
+++ app/src/main/java/com/example/myradio/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.myradio
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myradio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer : MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
