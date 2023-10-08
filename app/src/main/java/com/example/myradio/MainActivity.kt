82b1ec022c1c2c5498651daa3ad3783f9c3e7696
RanaHisham18 <hishamrana00@gmail.com>
2023-10-08 08:29:33 +0300

    add ViewBinding in screen 2

--- app/src/main/java/com/example/myradio/MainActivity.kt
+++ app/src/main/java/com/example/myradio/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.myradio
 
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myradio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mediaPlayer : MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var binding = ActivityMainBinding.inflate(layoutInflater)
--- app/src/main/java/com/example/myradio/RadioActivity.kt
+++ app/src/main/java/com/example/myradio/RadioActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myradio
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myradio.databinding.ActivityRadioBinding
 
 class RadioActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer : MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_radio)
+        var radioBinding = ActivityRadioBinding.inflate(layoutInflater)
+        setContentView(radioBinding.root)
     }
 }
